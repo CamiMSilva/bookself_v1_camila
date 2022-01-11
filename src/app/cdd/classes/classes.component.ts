092b2d521ef08354785f400c36dc7760ded465e9
Camila M. Silva <cms.mila@yahoo.com.br>
2022-01-11 14:11:34 -0300

    modularização MaterialUI e add serviço c endpoint

--- src/app/cdd/classes/classes.component.ts
+++ src/app/cdd/classes/classes.component.ts
@@ -7,18 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassesComponent implements OnInit {
 
-  livrosGenero: any[]=[
-    {_idGenero: "0", nomeGenero: "Generalidades", decimalGenero: "00", livrosGenero: 150},
-    {_idGenero: "1", nomeGenero: "Filosofia e Psicologia", decimalGenero: "00", livrosGenero: 29},
-    {_idGenero: "2", nomeGenero: "Religião", decimalGenero: "00", livrosGenero: 41},
-    {_idGenero: "3", nomeGenero: "Ciências Sociais", decimalGenero: "00", livrosGenero: 33},
-    {_idGenero: "4", nomeGenero: "Línguas", decimalGenero: "00", livrosGenero: 23},
-    {_idGenero: "5", nomeGenero: "Ciências Naturais e Matemáticas", decimalGenero: "00", livrosGenero: 77},
-    {_idGenero: "6", nomeGenero: "Tecnologia e Ciências Aplicadas", decimalGenero: "00", livrosGenero: 98},
-    {_idGenero: "7", nomeGenero: "Artes", decimalGenero: "00", livrosGenero: 99},
-    {_idGenero: "8", nomeGenero: "Literatura e Retórica", decimalGenero: "00", livrosGenero: 40},
-    {_idGenero: "9", nomeGenero: "Geografia, História e Biografia", decimalGenero: "00", livrosGenero: 14},
-  ];
+  livrosGenero: any[]=[];
   visaoColunas=['_idGenero', 'nomeGenero', 'decimalGenero']
 
   constructor() { }

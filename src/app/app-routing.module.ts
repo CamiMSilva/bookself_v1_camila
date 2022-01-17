8677ab6ac200ce22b084ab5030497c02ec423afc
Camila M. Silva <cms.mila@yahoo.com.br>
2022-01-17 14:13:34 -0300

    Add dashboard do Angular Material schematics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { FeedComponent } from './feed/feed.component';
+
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'cdd'},
+  {path: '', pathMatch: 'full', redirectTo: 'feed'},
+  {path: 'feed', component: FeedComponent  },
   {
-    path: 'cdd',
-    loadChildren: () => import('./cdd/cdd.module').then(m => m.CddModule)
+  path: 'cdd',
+    loadChildren: () => import('./cdd/cdd.module').then(c => c.CddModule)
   }
 ];
 
